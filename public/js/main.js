c1b68676415424b151a4a9a5a53e87af40f33cb0
“Erika <foglzerika@gmail.com>
2017-07-21 01:31:49 -0500

    Terminando las validaciones y agregando metodo post nuevo usua

--- public/js/main.js
+++ public/js/main.js
@@ -2,24 +2,33 @@ var ruta="http://localhost:3000/api/registerNumber"
 var tiempoRestante = 21;
 var textAreaTelefono = document.getElementById("code");
 var tiempo = document.getElementById("tiempo")
-
+var telefono = 0;
 var funcionCarousel = function(){
     $('.carousel.carousel-slider').carousel({fullWidth: true});    
 }
 
 var validaTelefono = function(){
-   var numeroTelefonico= document.getElementById("numeroTelefonico");
-   var bandera= numeroTelefonico.addEventListener("keydown",valida);
-    
+   var numero= $("#numeroTelefonico").val();
+   if (numero.length > 0 && numero.length == 10 )
+       {
+           var btnContinuar = document.getElementById("btnFormulario")
+            $(btnContinuar).removeClass("disabled");
+            btnContinuar.setAttribute("href","ingresaCodigo.html")
+
+       }else{
+           alert("inserte numero valido");
+           $("#numeroTelefonico").val('');
+           $("#checkBox").removeAttr("checked");
+       }
 }
 
 
 var cambia = function(){
     var btnCheck= document.getElementById("checkBox");
+    $("#checkBox").click(validaTelefono);
      btnCheck.setAttribute("checked","checked");
-    var btnContinuar = document.getElementById("btnFormulario")
-    $(btnContinuar).removeClass("disabled");
-    btnContinuar.setAttribute("href","ingresaCodigo.html")
+    validaTelefono();
+    
         
 }
 var deshabilitaTelefono = function (){
@@ -33,12 +42,18 @@ var habilitaTelefono = function(){
 
 var metodoPost = function(){
     var phoneNumeber =document.getElementById("numeroTelefonico").value
+    console.log(phoneNumeber)
     $.post(ruta,{
         "phone":phoneNumeber,
         "terms":true
     }).then(function (response){
         alert("Codigo: "+response.data.code);
-       localStorage.setItem("codigo",response.data.code);
+        console.log(response.data.phone)
+        $("#telefono").innerHTML=response.data.phone;
+        localStorage.setItem("telefono",response.data.phone);  
+        localStorage.setItem("codigo",response.data.code);
+      
+        
     }).catch(function(error){
         console.log(error);
     })
@@ -52,6 +67,7 @@ var metodoPost = function(){
 var valida = function (){
     contador++;
     console.log(contador);
+    return true;
 }
 
 var validaCodigo = function (){
@@ -69,7 +85,7 @@ var validaCodigo = function (){
   
 }
 var insertaTiempo = function (){
-    tiempo.innerHTML=tiempoRestante;    
+    $("#tiempo").innerHTML=tiempoRestante;    
 }
 var validaTiempo = function(){
     
@@ -81,15 +97,106 @@ var validaTiempo = function(){
         habilitaTelefono();
     }
 }
+var validaNombre = function (){
+    var nombre = $("#nombre").val();
+    localStorage.setItem("nombre",nombre);  
+    if(nombre.length >0 && nombre.length<20){
+        return true
+    }else{
+        return false 
+    }
+}
+var validaContrasena = function(){
+    var contrasena = $("#clave").val();
+    localStorage.setItem("clave",contrasena);  
+
+    if(contrasena.length >0 && contrasena.length===5){
+        return true
+    }else{
+        return false;
+    }
+}
+var validaFormulario = function(){
+    
+    var nombreUsuario = validaNombre();
+    var correoUsuraio = validaEmail();
+    var contrasena = validaContrasena();
+    if(nombreUsuario===true && correoUsuraio===true && contrasena ===true ){
+        
+            $("#creaCuenta").removeClass("disabled");
+
+    }
+    
+    
+    
+}
+function validaEmail(){
+       var correo = $("#correo").val();
+       localStorage.setItem("email",correo);  
+
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+        if (reg.test(correo) == false) 
+        {
+             
+            return false;
+        }
+
+        return true;
+
+}
+
+
+var obtieneNumero = function(){
+    var telefono = localStorage.getItem("telefono");
+    $("#numIngresado").text(telefono)
+}
+var insertaUsuario = function (){
+    var phone = localStorage.getItem("telefono");
+    var name = localStorage.getItem("nombre");
+    var mail = localStorage.getItem("email");
+    var passWord= localStorage.getItem("clave");
+     $.post("http://localhost:3000/api/createUser",{
+        "phone":phone,
+        "name":name,
+        "email":mail,
+        "password":passWord
+    }).then(function (response){
+         console.log(response);
+         
+         sigPantalla();
+
+        alert("datos dados dados de alta")
+        
+    })
+   
+    
+}
+var sigPantalla = function(){
+      window.location.href = "../bienvenida.html";
+
+}
 var cargaPagina = function(){
     funcionCarousel();
     $("#checkBox").click(cambia);
+    obtieneNumero();
+    $("#creaCuenta").click(insertaUsuario);
+    insertaTiempo();
     $("#btnFormulario").click(metodoPost);
     $('#codigoConfirmacion').submit(function (evt) {
         evt.preventDefault();
         validaCodigo();
     }); 
-    insertaTiempo();
+    $("#registroUsuario").submit(function(e){
+        e.preventDefault();
+    })
+    $("#registroUsuario").keypress(validaFormulario)
+       
+       
+    
+    
+    
+     
     
 }
 
